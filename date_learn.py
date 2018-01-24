a0ada5a00166c6726b08b7433280a3e1f0390ef4
mygethub-99 <tomakeitcount@gmail.com>
2018-01-23 21:25:46 -0600

    Update date_learn.py
    This worked. Now to add a function to remove repetitive code.

--- date_learn.py
+++ date_learn.py
@@ -2,36 +2,41 @@
 """
 Created on Mon Jan 22 12:22:06 2018
 
-@author: Me
+@author: OW4253
 """
 
 from dateutil.relativedelta import *
 from datetime import *
 
-#fopen = open('nsb_tracker.csv')
-#contents = fopen.readlines()
-#fopen.close()
 mklist = []
 today = date.today()
 
-with open('nsb_tracker.csv') as f:
+with open('datecompare.csv') as f:
     next(f)
     for line in f:
         lstarter = line.split(',')
-        ls = lstarter[5]
-        ls2 = lstarter[7]
-        ls3 = lstarter[8]
-        ls = (int(ls[0:4]), int(ls[4:6]), int(ls[6:8]))
-        ls = str(relativedelta(date(ls[0], ls[1], ls[2]), today))
-        ls2 = (int(ls2[0:4]), int(ls2[4:6]), int(ls2[6:8]))
-        ls2 = str(relativedelta(date(ls2[0], ls2[1], ls2[2]), today))
-        ls3 = (int(ls3[0:4]), int(ls3[4:6]), int(ls3[6:8]))
-        ls3 = str(relativedelta(date(ls3[0], ls3[1], ls3[2]), today))
-        block = (lstarter[1],ls[14:].strip(')'), ls2[14:].strip(')'), ls3[14:].strip(')'))
+        ls = lstarter[5] ; ls2= lstarter[6]; ls3 = lstarter[7]
+        olaq = lstarter[8] ; olcs = lstarter[10] ; olcf = lstarter[11]
+        
+        ls = date(int(ls[0:4]), int(ls[4:6]), int(ls[6:8]))
+        olaq = date(int(olaq[0:4]), int(olaq[4:6]), int(olaq[6:8]))
+        comp = str(relativedelta (olaq, ls))
+        
+        ls2 = date(int(ls2[0:4]), int(ls2[4:6]), int(ls2[6:8]))
+        olcs = date(int(olcs[0:4]), int(olcs[4:6]), int(olcs[6:8]))
+        comp2 = str(relativedelta (olcs, ls2))
+        
+        ls3 = date(int(ls3[0:4]), int(ls3[4:6]), int(ls3[6:8]))
+        olcf = date(int(olcf[0:4]), int(olcf[4:6]), int(olcf[6:8]))
+        comp3 = str(relativedelta (olcf, ls3))
+        
+        block = (lstarter[1],comp[14:].strip(')'), comp2[14:].strip(')'), \
+        comp3[14:].strip(')'), lstarter[8], lstarter[10], lstarter[13])
         mklist.insert(0, block)
         
-with open('mstracker.txt', 'w') as fs:
-    fs.write("'FA_Code' 'SiteAq' 'ConStart' 'ConFinish'")
+with open('deltatracker.txt', 'w') as fs:
+    fs.write("'FA_Code' 'SiteAqDel' 'ConStarDel' 'ConFinDel' 'SA' 'ConStar'\
+    'On-Air'")
     fs.write("\n")
     for i in mklist:
         i = str(i).strip('()')
