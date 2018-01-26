45b3f51a51925d799269d4654d3b4feac94248c6
mygethub-99 <tomakeitcount@gmail.com>
2018-01-25 21:56:31 -0600

    Update datecompatedimproved.py
    Removed the function and just used a loop.

--- datecompatedimproved.py
+++ datecompatedimproved.py
@@ -2,38 +2,39 @@
 """
 Created on Mon Jan 22 12:22:06 2018
 
-@author: OW
+@author: OW4253
 """
 
 from dateutil.relativedelta import *
 from datetime import *
-
-mklist = []
+tofile = []
 
 with open('datecompare.csv') as f:
     next(f)
     for line in f:
+        trklist = []
         lstarter = line.split(',')
         list1 = (lstarter[5] , lstarter[6] , lstarter[7])
         list2 = (lstarter[8] , lstarter[10] , lstarter[11])
         list3 = (lstarter[1], lstarter[10], lstarter[13])
         
-        def repeater(list1, list2, list3):
-            for i, y, z in zip(list1, list2, list3):
-                ls = date(int(i[0:4]), int(i[4:6]), int(i[6:8]))
-                olaq = date(int(y[0:4]), int(y[4:6]), int(y[6:8]))
-                comp = str(relativedelta (olaq, ls))
-                comb = (str(z), comp[14:], )
-                mklist.insert(0, comb)
-        repeater(list1, list2, list3)
-        print (mklist)
-        
+        #Loop to organize list for relative date
+        for i, y, z in zip(list1, list2, list3):
+            ls = date(int(i[0:4]), int(i[4:6]), int(i[6:8]))
+            olaq = date(int(y[0:4]), int(y[4:6]), int(y[6:8]))
+            comp = str(relativedelta (olaq, ls))
+            #comb = (str(z), comp[14:])
+            trklist.insert(0, (str(z), comp[14:]))
+            if len(trklist) == 3:
+                tofile.insert(0, [trklist])
+                del(trklist)
+            
 with open('deltplay.txt', 'w') as fs:
     fs.write("'FA_Code' 'SiteAqDel' 'ConStarDel' 'ConFinDel' 'SA' 'ConStar'\
     'On-Air'")
     fs.write("\n")
-    for i in mklist:
-        i = str(i).strip('()')
-        i = i.replace(",", "")
+    for i in tofile:
+        i = str(i).strip('[]')
+        #i = i.replace(",", "")
         fs.write("{}\n".format(i))
-fs.close()
+fs.close() 
